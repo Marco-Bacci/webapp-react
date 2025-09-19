15206a9f665bf8f2967f6c16660d08dc3da47bc5
Marco Bacci <marcobacci94@gmail.com>
2025-09-19 19:49:05 +0200

    feat: map movies

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -8,6 +8,7 @@ const Homepage = () => {
       .get("http://localhost:3000/movies")
       .then((resp) => {
         setMovies(resp.data);
+        console.log(resp.data);
       })
       .catch((err) => {
         console.log(err);
@@ -18,15 +19,26 @@ const Homepage = () => {
 
   return (
     <div className="container">
-      <div className="row gy-4">
+      <div className="row">
         <div className="col-12">
           <h2 className="text-center my-3">Homepage</h2>
         </div>
-        <div className="col-12 col-md-6 col-lg-4">
-          <div className="movie-card">
-            <img src="./imgs/inception.jpg" alt="" />
-            <div className="overlay"></div>
-          </div>
+        <div className="row gy-4">
+          {movies.map((movie) => {
+            return (
+              <div className="col-12 col-md-6 col-lg-4" key={movie.id}>
+                <div className="movie-card">
+                  <img src={movie.image} alt="movie" />
+                  <div className="overlay">
+                    <h2>{movie.title}</h2>
+                    <p>{movie.director}</p>
+                    <p>{movie.genre}</p>
+                    <p>{movie.release_year}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
