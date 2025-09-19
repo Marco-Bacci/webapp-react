b2d09439d237f526c875284e2a3424e0781ddef7
Marco Bacci <marcobacci94@gmail.com>
2025-09-19 18:58:11 +0200

    upd: homepage structure

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -1,10 +1,20 @@
-
 const Homepage = () => {
   return (
-    <div>
-      <h2>Homepage</h2>
+    <div className="container">
+      <div className="row gy-4">
+        <div className="col-12">
+          <h2 className="text-center my-3">Homepage</h2>
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="movie-card">
+            <img src="./imgs/inception.jpg" alt="" />
+            <div className="overlay"></div>
+          </div>
+        </div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
