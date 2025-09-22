ba2e2b9bda6282ebcaaf209f57c8beb13212446f
Marco Bacci <marcobacci94@gmail.com>
2025-09-22 18:18:30 +0200

    feat: add logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="d-flex justify-content-between align-items-center">
-      <h1>Sexy Movies</h1>
-      <Link className="home text-light" to="/"><i className="fa-solid fa-house-chimney"></i></Link>
+      <div className="logo d-flex">
+        <img src="/imgs/film-reel-fill.svg" alt="" />
+        <h1>Sexy Movies</h1>
+      </div>
+      <Link className="home text-light" to="/">
+        <i className="fa-solid fa-house-chimney"></i>
+      </Link>
     </header>
   );
 };
