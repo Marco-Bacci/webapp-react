0384dc2b6458645d4ae80eea598b8c8c605cf575
Marco Bacci <marcobacci94@gmail.com>
2025-09-22 13:52:02 +0200

    feat: add link to MovieCard

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -1,17 +1,20 @@
-const MovieCard = ({movie}) => {
-  const {image, title, director, release_year, abstract} = movie
+import { Link } from "react-router-dom";
+const MovieCard = ({ movie }) => {
+  const { image, title, director, release_year, abstract } = movie;
   return (
-    <div className="movie-card">
-      <img src={`./imgs/${image}`} alt="movie" />
-      <div className="overlay text-center p-3">
-        <h2>{title}</h2>
-        <p>{director}</p>
-        <p>
-          genre: {movie.genre} year: {release_year}
-        </p>
-        <p>{abstract}</p>
+    <Link to = "http://localhost:5173/movies/:id">
+      <div className="movie-card">
+        <img src={`./imgs/${image}`} alt="movie" />
+        <div className="overlay text-center p-3">
+          <h2>{title}</h2>
+          <p>{director}</p>
+          <p>
+            genre: {movie.genre} year: {release_year}
+          </p>
+          <p>{abstract}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
