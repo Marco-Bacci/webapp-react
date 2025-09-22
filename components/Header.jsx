476b01512a58997acd2b71c1f108d20643726687
Marco Bacci <marcobacci94@gmail.com>
2025-09-22 15:02:37 +0200

    feat: import fontawesome and change link for homepage

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="d-flex justify-content-between align-items-center">
-      <h1>Movieteca</h1>
-      <Link className="home" to="http://localhost:5173/">home</Link>
+      <h1>Sexy Movies</h1>
+      <Link className="home" to="http://localhost:5173/"><i class="fa-solid fa-house-chimney"></i></Link>
     </header>
   );
 };
