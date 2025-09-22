da1346e3c6fa80a78af7595ceccea15725ccc051
Marco Bacci <marcobacci94@gmail.com>
2025-09-22 14:47:41 +0200

    feat: add into header the link for the homepage

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,11 @@
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <header>
+    <header className="d-flex justify-content-between align-items-center">
       <h1>Movieteca</h1>
-
+      <Link className="home" to="http://localhost:5173/">home</Link>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
