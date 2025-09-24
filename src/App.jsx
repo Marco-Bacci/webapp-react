f4ed7259b68e33131760f473c84dea180d9f2631
Marco Bacci <marcobacci94@gmail.com>
2025-09-24 15:05:40 +0200

    feat: create Context and import it into app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import MovieDetail from "../pages/MovieDetail";
 import DefaultLayout from "../layout/DefaultLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "../pages/NotFound";
-
+import GlobalContext from "./contexts/GlobalContext";
+import { useState } from "react";
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
   return (
-    <>
-      <BrowserRouter>
+    <GlobalContext.Provider value = {{isLoading, setIsLoading}}>
+      <BrowserRouter> 
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route path="/" element={<Homepage />} />
@@ -16,9 +18,8 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </GlobalContext.Provider>
   );
 }
-
 
 export default App;
